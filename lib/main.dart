8ec9912c971bf6454c46b742da1bfdbf472824b2
dimysenpai <dimitrymabom@gmail.com>
2023-05-23 14:54:11 +0100

    init first app flutter 'todolist'

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/screens/home_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TodoList',
+      home: const HomeScreen(),
+    );
+  }
+}
